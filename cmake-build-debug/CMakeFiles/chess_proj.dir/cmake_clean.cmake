3a41d35430a256efbf2239b80419c35a7eba5a41
JosephThrelkeld <65369227+JosephThrelkeld@users.noreply.github.com>
2023-09-25 15:28:36 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/chess_proj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_proj.dir/main.cpp.obj"
+  "chess_proj.exe"
+  "chess_proj.exe.manifest"
+  "chess_proj.lib"
+  "chess_proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
